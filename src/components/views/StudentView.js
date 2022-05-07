7ba8f60a785767ade1d9af124de1288e4def0fb0
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2022-05-07 13:26:21 -0400

    Added to single student view by adding gpa, image, message if not enrolled at a campus, a link to the single view campus if they are enrolled, and email to StudentView.js

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,8 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -11,7 +13,21 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <h2>{student.email}</h2>
+      <img src={student.imageUrl} alt="Student Profile"/>
+      {student.campus === null ? (
+        <div>
+          <h3>{student.firstname + " " + student.lastname} is not enrolled at a school.</h3>
+          <h4>GPA: N/A</h4>
+        </div>
+      ) : (
+        <div>
+          <Link to={`/campus/${student.campus.id}`}>
+            <h3>{student.campus.name}</h3>
+          </Link>
+          <h4>GPA: {student.gpa}</h4>
+        </div>
+      )}
     </div>
   );
 
