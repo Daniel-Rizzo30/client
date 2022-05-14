0abac0d4ae481014b5c33453951906d981a57a2b
Daniel.Rizzo30 <rimshot816@DESKTOP-2UTBUI6.localdomain>
2022-05-14 15:38:43 -0400

    Added more to css - an image on the front page, Edit form css taken from the 'New' forms

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,11 +4,28 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import { makeStyles } from '@material-ui/core/styles';
+
+// Create styling for the input form
+const useStyles = makeStyles( () => ({
+  image: {
+    maxWidth: '60%',
+    maxHeight: '50%',
+  },
+}));
+
 const HomePageView = () => {
+  const classes = useStyles();
   // Render Home page view
   return (
     <div >
       <h1>Home Page</h1>
+      <br />
+      <br />
+      <img className={classes.image} 
+      alt="City College" 
+      src="https://www.ccny.cuny.edu/sites/default/files/styles/section_background_image_1440x900/public/2019-08/carlosparker_harrishall.jpg"
+      />
     </div>
   );    
 }
